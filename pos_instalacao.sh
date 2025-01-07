f617acc891ca3fb805e3bdc3d2ff9bc12a84daa5
rbgameslinux <rbgameslinux@gmail.com>
2025-01-06 21:15:55 -0300

    Update pos_instalacao.sh
    mais correções

--- pos_instalacao.sh
+++ pos_instalacao.sh
@@ -56,39 +56,7 @@ if [ "$resposta" == "s" ]; then
 echo
 echo
 echo   
-    sudo pacman -Syu --noconfirm ; sudo pacman -R dunst --noconfirm ; sudo pacman -S --needed\
-    it wget unzip gum rofi wofi unrar okular gwenview\
-    waybar swww dolphin dolphin-plugins ark firefox\
-    loupe xdg-desktop-portal xdg-desktop-portal-gnome\
-    xdg-desktop-portal-gtk xdg-desktop-portal-hyprland\
-    xdg-desktop-portal-wlr xdg-user-dirs xdg-user-dirs-gtk\
-    xdg-utils archlinux-xdg-menu btop ttf-font-awesome noto-fonts\
-    noto-fonts-emoji noto-fonts-extra ttf-firacode-nerd\
-    ttf-jetbrains-mono-nerd swayidle polkit-gnome bash-completion\
-    ntfs-3g ffmpegthumbnailer ffmpegthumbs volumeicon pavucontrol\
-    pamixer notification-daemon wl-clipboard wayland-utils\
-    clinfo alsa-utils  imagemagick adw-gtk-theme breeze breeze5\
-    breeze-gtk breeze-icons nwg-look swaylock\
-    kio kde-cli-tools ntfs-3g exfat-utils dosfstools btrfs-progs\
-    xfsprogs jfsutils f2fs-tools reiserfsprogs nilfs-utils udftools\
-    e2fsprogs kitty gedit gparted mangohud bash-completion\
-    telegram-desktop discord wine wine-gecko wine-mono winetricks\
-    curl deluge-gtk vulkan-radeon libva-mesa-driver vulkan-icd-loader\
-    lib32-mesa lib32-vulkan-radeon lib32-vulkan-icd-loader\
-    lib32-libva-mesa-driver mesa-demos xorg-xdpyinfo amd-ucode\
-    mesa-utils glfw-wayland vulkan-tools mesa-vdpau lib32-mesa-vdpau\
-    lvm2 zip timeshift dosfstools linux-lts-headers linux-zen-headers\
-    steam lutris gimp antimicrox celluloid mpv vlc android-tools hyprutils\
-    hyprland-qtutils qt6-5compat qt6-declarative qt6-svg sddm gnome-disk-utility\
-    gnome-calendar gnome-calculator ksnip --noconfirm 
-
-elif [ "$resposta" == "n" ]; then
-        # Executa o comando se a resposta for "não"
-    echo "Você escolheu NÃO. Executando a instalação com xorg"
-echo
-echo
-echo    
-    sudo pacman -Syu --noconfirm ; sudo pacman -S --needed\
+ sudo pacman -Syu --noconfirm ; sudo pacman -R dunst --noconfirm ; sudo pacman -S --needed\
     git wget unzip gum rofi wofi unrar okular gwenview\
     waybar swww dolphin dolphin-plugins ark firefox\
     loupe xdg-desktop-portal xdg-desktop-portal-gnome\
@@ -100,23 +68,21 @@ echo
     ntfs-3g ffmpegthumbnailer ffmpegthumbs volumeicon pavucontrol\
     pamixer notification-daemon wl-clipboard wayland-utils\
     clinfo alsa-utils  imagemagick adw-gtk-theme breeze breeze5\
-    breeze-gtk breeze-icons nwg-look swaylock\
-    kio kde-cli-tools sddm xorg network-manager-applet ntfs-3g\
-    exfat-utils dosfstools btrfs-progs xfsprogs jfsutils f2fs-tools\
-    reiserfsprogs nilfs-utils udftools e2fsprogs gedit gparted\
-    mangohud bash-completion telegram-desktop discord wine\
-    wine-gecko wine-mono winetricks curl deluge-gtk kitty\
-    vulkan-radeon libva-mesa-driver vulkan-icd-loader lib32-mesa\
-    lib32-vulkan-radeon lib32-vulkan-icd-loader lib32-libva-mesa-driver\
-    mesa-demos xorg-xdpyinfo amd-ucode mesa-utils glfw-wayland\
-    mesa llvm lib32-llvm vulkan-tools xf86-video-amdgpu mesa-vdpau\
-    lib32-mesa-vdpau lvm2 kitty zip timeshift dosfstools lutris\
-    linux-lts-headers linux-zen-headers steam mesa gimp antimicrox\
-    celluloid mpv vlc android-tools hyprutils hyprland-qtutils qt6-5compat\
-    qt6-declarative qt6-svg gnome-disk-utility gnome-calendar\
-    gnome-calculator ksnip --noconfirm
+    breeze-gtk breeze-icons nwg-look
+    kio kde-cli-tools exfat-utils dosfstools btrfs-progs\
+    xfsprogs jfsutils f2fs-tools reiserfsprogs nilfs-utils udftools\
+    e2fsprogs kitty gedit gparted mangohud\
+    telegram-desktop discord wine wine-gecko wine-mono winetricks\
+    curl deluge-gtk vulkan-radeon libva-mesa-driver vulkan-icd-loader\
+    lib32-mesa lib32-vulkan-radeon lib32-vulkan-icd-loader\
+    lib32-libva-mesa-driver mesa-demos xorg-xdpyinfo amd-ucode\
+    mesa-utils glfw-wayland vulkan-tools mesa-vdpau lib32-mesa-vdpau\
+    lvm2 zip timeshift dosfstools linux-lts-headers linux-zen-headers\
+    steam lutris gimp antimicrox celluloid mpv vlc android-tools hyprutils\
+    hyprland-qtutils qt6-5compat qt6-declarative qt6-svg gnome-disk-utility\
+    gnome-calendar gnome-calculator ksnip --noconfirm 
 
-    #instalar o paru p/ter suporte ao aur.
+    #instalar o yay p/ter suporte ao aur.
 echo
 echo
 echo
@@ -148,17 +114,199 @@ echo
     qt5ct-kde \
     qt6ct-kde \
     grimblast \
-    protonup-qt-bin \
+    swaylock-effects \
     swaync-git \
-    ventoy-bin \
     wlogout \
+    protonup-qt-bin \
+    ventoy-bin \
     obs-studio-tytan652 \
     obs-vkcapture \
     winff \
     droidcam \
     v4l2loopback-dc-dkms \
-    google-chrome \
-    brave-bin \
+   # google-chrome \
+   # brave-bin \
+    stremio \
+    opencl-amd \
+    deckboard-bin \
+    r-linux \
+    heroic-games-launcher-bin \
+    visual-studio-code-bin \
+    jamesdsp-pipewire-bin \
+    terabox-bin \
+    clipman \
+    clipse
+echo
+echo
+echo
+        # Obter o nome do usuário atual
+    usuario=$(whoami)
+
+        # Adiciona o usuário ao grupo 'wheel'
+    sudo usermod -aG wheel "$usuario"
+
+        #Só rodar no terminal
+    xdg-user-dirs-update 
+
+        #coloca o dolphin com padrão do sistema
+    xdg-mime default org.kde.dolphin.desktop inode/directoryxdg-mime default org.kde.dolphin.desktop inode/directory 
+
+        #Habilita o sddm
+
+    systemctl enable sddm.service
+echo
+echo "criando pastas e dando permissões"      
+echo
+sleep 5
+echo
+    sudo mkdir /mnt/Rodrigo000
+    sudo mkdir /mnt/Rodrigo001
+    sudo mkdir /mnt/Rodrigo002
+    sudo mkdir /mnt/Rodrigo003
+    sudo mkdir /mnt/Rodrigo004
+    sudo mkdir /mnt/Rodrigo005
+    sudo mkdir /mnt/Rodrigo006
+sleep 3
+echo
+    sudo chmod + /mnt/Rodrigo000
+    sudo chmod + /mnt/Rodrigo001
+    sudo chmod + /mnt/Rodrigo002
+    sudo chmod + /mnt/Rodrigo003
+    sudo chmod + /mnt/Rodrigo004
+    sudo chmod + /mnt/Rodrigo005
+    sudo chmod + /mnt/Rodrigo006
+sleep 3
+echo
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo000
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo001
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo002
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo003
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo004
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo005
+    sudo chown rodrigo:rodrigo /mnt//Rodrigo006
+sleep 3
+echo
+echo "Finalizado a criação de pastas em /mnt"
+echo
+sleep 2
+echo
+echo "Extraindo configurações nas suas devidas pastas"
+echo
+sleep 3
+
+    cd $HOME/pos_install
+    
+    sudo tar -xzvf simple-sddm-2.tar.gz -C /usr/share/sddm/themes/
+    sudo tar -xzvf sddm.conf.tar.gz -C /etc/
+    tar -xzvf rodrigo_configs.tar.gz
+echo    
+sleep 5
+echo 
+    cp -r rodrigo_configs/* ~/.config
+    
+echo "Descompactação finalizada com sucesso"
+echo
+
+sleep 7
+echo
+cd $HOME/
+echo
+echo
+
+rm -fr pos_install
+
+echo "Vamos reiniciar a sua maquina e após isso estára pronta para uso"
+echo "Obrigado"
+echo
+echo "Reiniciando" 
+echo
+echo
+echo
+echo "Pressione Enter para reiniciar, ou CTRL+C para cancelar."
+read -p ""
+echo
+echo
+sudo reboot
+    
+
+elif [ "$resposta" == "n" ]; then
+        # Executa o comando se a resposta for "não"
+    echo "Você escolheu NÃO. Executando a instalação com xorg"
+echo
+echo
+echo    
+    sudo pacman -Syu --noconfirm ; sudo pacman -S --needed\
+    git wget unzip gum rofi wofi unrar okular gwenview\
+    waybar swww dolphin dolphin-plugins ark firefox\
+    loupe xdg-desktop-portal xdg-desktop-portal-gnome\
+    xdg-desktop-portal-gtk xdg-desktop-portal-hyprland\
+    xdg-desktop-portal-wlr xdg-user-dirs xdg-user-dirs-gtk\
+    xdg-utils archlinux-xdg-menu btop ttf-font-awesome noto-fonts\
+    noto-fonts-emoji noto-fonts-extra ttf-firacode-nerd\
+    ttf-jetbrains-mono-nerd swayidle polkit-gnome bash-completion\
+    ntfs-3g ffmpegthumbnailer ffmpegthumbs volumeicon pavucontrol\
+    pamixer notification-daemon wl-clipboard wayland-utils\
+    clinfo alsa-utils  imagemagick adw-gtk-theme breeze breeze5\
+    breeze-gtk breeze-icons nwg-look\
+    kio kde-cli-tools sddm xorg network-manager-applet\
+    exfat-utils dosfstools btrfs-progs xfsprogs jfsutils f2fs-tools\
+    reiserfsprogs nilfs-utils udftools e2fsprogs kitty gedit gparted\
+    mangohud telegram-desktop discord wine\
+    wine-gecko wine-mono winetricks curl deluge-gtk\
+    vulkan-radeon libva-mesa-driver vulkan-icd-loader lib32-mesa\
+    lib32-vulkan-radeon lib32-vulkan-icd-loader lib32-libva-mesa-driver\
+    mesa-demos xorg-xdpyinfo amd-ucode mesa-utils glfw-wayland\
+    mesa llvm lib32-llvm vulkan-tools xf86-video-amdgpu mesa-vdpau\
+    lib32-mesa-vdpau lvm2 zip timeshift dosfstools lutris\
+    linux-lts-headers linux-zen-headers steam gimp antimicrox\
+    celluloid mpv vlc android-tools hyprutils hyprland-qtutils qt6-5compat\
+    qt6-declarative qt6-svg gnome-disk-utility gnome-calendar\
+    gnome-calculator ksnip --noconfirm
+
+    #instalar o yay p/ter suporte ao aur.
+echo
+echo
+echo
+echo "Instalando o suporte ao aur-helper com yay"
+sleep 5
+echo
+echo
+cd $HOME/
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si --noconfirm
+    cd $HOME/
+echo
+echo
+sleep 7
+echo
+echo
+    #Instalar pacotes do aur com paru 
+echo "Por questões de segurança e falhas"
+echo "eu opitei por confirmar manualmente todos os pacotes do aur"
+echo
+sleep 10
+echo
+echo
+    yay -S \
+    swayosd-git \
+    waypaper \
+    hyprswitch \
+    qt5ct-kde \
+    qt6ct-kde \
+    grimblast \
+    swaylock-effects \
+    swaync-git \
+    wlogout \
+    protonup-qt-bin \
+    ventoy-bin \
+    obs-studio-tytan652 \
+    obs-vkcapture \
+    winff \
+    droidcam \
+    v4l2loopback-dc-dkms \
+   # google-chrome \
+   # brave-bin \
     stremio \
     opencl-amd \
     deckboard-bin \
